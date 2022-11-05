e7a582b0cd292846804b9c398c8f98de33429da4
kabutoa <yinyun957@163.com>
2022-11-05 23:39:01 +0800

    feat: add normalize.css

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+// import css
+import "normalize.css";
+import "@/assets/css/index.scss";
+
 Vue.config.productionTip = false;
 
 new Vue({
